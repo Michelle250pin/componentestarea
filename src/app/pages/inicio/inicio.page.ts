c56eb928ae52fa3695b770cb0e3bbde640b38033
Michelle250pin <michellepincolorado@gmail.com>
2024-06-06 20:17:17 -0500

    add: cambiosjueves

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ComponentsModule } from '../../components/components.module';
+import { MenuController } from '@ionic/angular'; 
+import { DataService } from '../../services/data.service'; 
+import { Observable } from 'rxjs'; 
 //crear una interface para definir propiedades que el componente lista va
 
 interface Componente {
@@ -15,42 +18,13 @@ styleUrls: ['./inicio.page.scss'],
 export class InicioPage implements OnInit {
 //crear nuevo listado
 //componentes: any[] = [];
-componentes: Componente[] = [
-    { 
-        icon: 'american-football-outline', 
-        name: 'Action Sheet', 
-        redirectTo: '/action-sheet' 
-      }, 
-      { 
-        icon: 'alert-circle-outline', 
-        name: 'Alert', 
-        redirectTo: '/alert' 
-      }, 
-      { 
-        icon: 'body-outline', 
-        name: 'Avatar', 
-        redirectTo: '/avatar' 
-      }, 
-      { 
-        icon: 'calendar-number-outline', 
-        name: 'Datetime', 
-        redirectTo: '/datetime' 
-      }, 
-      { 
-        icon: 'pencil-outline', 
-        name: 'Input', 
-        redirectTo: '/input' 
-      }, 
-
-
-
-      
-      
-
-];
-constructor() { }
-ngOnInit() {
-}   
-
-
-}
+componentes: Observable <Componente[]>; 
+constructor(private menuCtrl: MenuController) { } 
+ 
+  ngOnInit() { 
+  } 
+  mostrarMenu() { 
+    this.menuCtrl.open('first'); 
+  } 
+ 
+} 
